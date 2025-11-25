94a1949e30cc1864332d292a6eb009d70709e77e
Gustav O <ac75309@skola.goteborg.se>
2025-11-25 11:24:12 +0100

    First add

--- src/main/java/se/yrgo/project/HangManGame.java
+++ src/main/java/se/yrgo/project/HangManGame.java
@@ -1,4 +1,62 @@
 package se.yrgo.project;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class HangManGame {
+
+    public void welcome() {
+        System.out.println("Welcome to the Hangman Game!\n");
+        System.out.println("----------------------------------------");
+        System.out.println("1. To start a new game, enter your name.");
+        System.out.println("2. To end the game at any time, press 0.");
+        System.out.println("----------------------------------------");
+        System.out.println("Rules: ");
+        System.out.println(" - Guess only one letter at a time, the program will tell you if it is correct or not.\n - Only letters from the english alphabet is acceptable.");
+        System.out.println(" - For every guess your total guesses will increase by one, less is better!");
+        System.out.println(" - For every wrong guess, you will lose one life, you got 10 lives to start with");
+        System.out.println(" - The game will end if you lose all lives.");
+        System.out.println(" - When the game ends you will receive your score, the lower the better!\n");
+        System.out.println("GOOD LUCK!");
+
+    }
+
+    public List<Character> getWordList() {
+        List<String> wordList = List.of("Java");
+        Words word = new Words(wordList);
+
+        String chosenWord = word.getRandomWord();
+
+        List<Character> wordAsList = word.wordToLetters(chosenWord);
+
+        return wordAsList;
+    }
+
+    public void startGame() {
+
+        System.out.print("Please insert your name: ");
+
+        try (Scanner input = new Scanner(System.in)) {
+            String name = input.nextLine().trim();
+            Player player = new Player(name);
+
+            System.out.println("Welcome " + player.getName() + "!");
+
+            System.out.print("Your word is: ");
+
+            for (int i = 0; i <= getWordList().size(); i++) {
+                System.out.print(" _ ");
+            }
+            System.out.print("\n\nLet's start, take a guess: ");
+            Character letter = input.next().charAt(0);
+            player.guess(letter);
+
+            if (getWordList().contains(letter)) {
+                System.out.println("Korrekt!");
+            } else {
+                System.out.println("Felaktigt!");
+            }
+        }
+
+    }
 }
--- src/main/java/se/yrgo/project/Player.java
+++ src/main/java/se/yrgo/project/Player.java
@@ -1,4 +1,53 @@
 package se.yrgo.project;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Player {
+    private final String name;
+    private int lives;
+    private Set<Character> guesses;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.lives = 10;
+        this.guesses = new HashSet<>();
+        this.score = 0;
+    }
+    //The guess method guesses a letter, checking if the letter has been guessed before.
+    // If the letter has been guessed before, @return false, if the letter has not appeared yet,
+    // add it to the Hashset.
+    public boolean guess(Character letter) {
+        letter = Character.toLowerCase(letter);
+        if (guesses.contains(letter))
+        {return false;}
+        else {
+            guesses.add(letter);
+            return true;
+        }
+    }
+    public int totalGuesses() {
+        return guesses.size();
+    }
+    public void wrongGuess() {
+        lives--;
+    }
+    public void correctGuess() {
+        score++;
+    }
+
+    public Set<Character> getGuesses() {
+        return guesses;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getLives() {
+        return lives;
+    }
+    public boolean isGameOver() {
+        return lives == 0;
+    }
 }
