d386418654ab5e5069dd55ee8ace5e99526daf64
Gustav O <ac75309@skola.goteborg.se>
2025-12-02 12:56:56 +0100

    Changes in hangmangame.java

--- src/main/java/se/yrgo/project/HangManGame.java
+++ src/main/java/se/yrgo/project/HangManGame.java
@@ -2,6 +2,7 @@ package se.yrgo.project;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class HangManGame {
 
@@ -12,7 +13,8 @@ public class HangManGame {
         System.out.println("2. To end the game at any time, press 0.");
         System.out.println("----------------------------------------");
         System.out.println("Rules: ");
-        System.out.println(" - Guess only one letter at a time, the program will tell you if it is correct or not.\n - Only letters from the english alphabet is acceptable.");
+        System.out.println(" - Guess only one letter at a time, the program will tell you if it is correct or not.");
+        System.out.println(" - Only letters from the english alphabet is acceptable.");
         System.out.println(" - For every guess your total guesses will increase by one, less is better!");
         System.out.println(" - For every wrong guess, you will lose one life, you got 10 lives to start with");
         System.out.println(" - The game will end if you lose all lives.");
@@ -20,78 +22,74 @@ public class HangManGame {
         System.out.println("GOOD LUCK!");
     }
 
-    public List<Character> getWordAsList() {
-        List<String> wordList = List.of("jenkins", "java", "groovy", "maven", "python");
+    public void displayWord(List<Character> wordList, Set<Character> guessedLetters) {
+        for (Character c : wordList) {
+            if (guessedLetters.contains(Character.toLowerCase(c))) {
+                System.out.print(c + " ");
+            } else {
+                System.out.print("_ ");
+            }
+        }
+        System.out.println();
+    }
+
+    private boolean isWordSolved(List<Character> word, Set<Character> guesses) {
+        for (char c : word) {
+            if (!guesses.contains(Character.toLowerCase(c))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Character> getWordList() {
+        List<String> wordList = List.of("Java", "Python", "Maven", "Jenkins", "Gradle");
         Words word = new Words(wordList);
-
         String chosenWord = word.getRandomWord();
-
-        List<Character> wordAsList = word.wordToLetters(chosenWord);
-
-        return wordAsList;
+        return word.wordToLetters(chosenWord);
     }
 
     public void startGame() {
-
         System.out.print("Please insert your name: ");
 
         try (Scanner input = new Scanner(System.in)) {
-
             String name = input.nextLine().trim();
             Player player = new Player(name);
 
-            System.out.println("Welcome " + player.getName() + "!\n");
+            List<Character> wordList = getWordList();
+            Set<Character> guessedLetters = player.getGuesses();
 
-            // Spara randomordet i en variabel
-            List<Character> word = getWordAsList();
+            System.out.println("Welcome " + player.getName() + "!");
+//            System.out.print("Your word is: ");
+//            for (int i = 0; i < wordList.size(); i++) {
+//                System.out.print(" _ ");
+//            }
+//            System.out.println("\n");
 
-            // Skapa en lista med underscore baserat på längden av ord oavsett ord
-            char[] maskedWord = new char[word.size()];
-            for (int i = 0; i < maskedWord.length; i++) {
-                maskedWord[i] = '_';
+            while (!isWordSolved(wordList, guessedLetters) && player.getLives() > 0) {
+                System.out.println("\nYour word is: ");
+                displayWord(wordList, guessedLetters);
+                System.out.print("\nTake a guess: ");
+                char guess = input.next().toLowerCase().charAt(0);
+
+                if (player.guess(guess)) {
+                    if (wordList.contains(guess)) {
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println("Wrong!");
+                        player.wrongGuess();
+                    }
+                } else {
+                    System.out.println("You already guessed that letter!");
+                }
             }
 
-            // skriv ut array med String.valueOf()
-            System.out.print("Your word is: " + String.valueOf(maskedWord));
-
-            while (true) {
-
-                if (player.getLives() <= 0) {
-                    System.out.println("\n OUCH! The man has been hanged, you lost!");
-                    break;
-                }
-                // (new String) skapar en sträng av Char array, breaka loopen om ordet är gissat!
-                if (!new String(maskedWord).contains("_")) {
-                    System.out.println("\nCongratz! You got the word: " + String.valueOf(maskedWord));
-                    break;
-                }
-
-                System.out.print("\nLet's try, take a guess: ");
-                char letter = input.next().toLowerCase().charAt(0);
-
-                boolean isFound = false;
-
-                // jämför underscore array med bokstav och ersätt underscore med bokstav.
-                for (int i = 0; i < word.size(); i++) {
-                    if (Character.toLowerCase(word.get(i)) == letter) {
-                        maskedWord[i] = word.get(i);
-                        isFound = true;
-                    }
-                }
-
-                if (isFound) {
-                    System.out.println("\nThat's correct!");
-                    System.out.println("Guessed letters: " + player.getGuesses());
-                } else {
-                    System.out.println();
-                    player.wrongGuess();
-                    System.out.println("Wrong! Guess again. " + player.getLives() + " lives remaining.");
-                    player.guess(letter);
-                    System.out.println("Guessed letters: " + player.getGuesses());
-                }
-
-                System.out.println("\nYour word is: " + String.valueOf(maskedWord));
+            if (isWordSolved(wordList, guessedLetters)) {
+                System.out.println("You won!");
+            } else {
+                System.out.println("Game over! Ordet var: " + wordList);
             }
         }
     }
+
 }
